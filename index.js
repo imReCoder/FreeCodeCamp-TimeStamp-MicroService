47bc75717660a71682f90974addaadea0861b0e0
Ranjit Kumar <ranjitkumar448@yahoo.com>
2022-07-22 14:48:57 +0530

    completed

---
+++ index.js
@@ -0,0 +1,46 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/:date", function (req, res) {
+  try {
+    let unix, utc;
+    const para = req.params.date;
+    const someNum = Number(para);
+    if (Number.isNaN(someNum)) {
+      unix = new Date(para).getTime();
+    } else {
+      unix = someNum;
+    }
+    utc = new Date(unix).toUTCString();
+
+    res.json({ unix, utc });
+  } catch (e) {
+    res.json({ error: "Invalid Date" });
+  }
+});
+
+
+const PORT = 3500;
+// listen for requests :)
+const listener = app.listen(PORT, function () {
+  console.log('Your app is listening on port ' + PORT);
+});
